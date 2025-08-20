c205827150662db17beb3b1734a49e76f0a11476
Ziad84 <ziad.albalwi1@gmail.com>
2025-08-20 18:04:54 +0300

    feat: updated the application name

--- src/main/java/com/RentalApplication/rent/service/RentServiceApplication.java
+++ src/main/java/com/RentalApplication/rent/service/RentServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RtApplication {
+public class RentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RtApplication.class, args);
+		SpringApplication.run(RentServiceApplication.class, args);
 	}
 
 }
